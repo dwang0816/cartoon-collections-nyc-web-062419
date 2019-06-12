fa366cb0fd09f06ded3bcbb8d8db8a823332e70f
dennis wang <dwang0816@gmail.com>
2019-06-12 03:38:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,21 +1,3 @@
 def roll_call_dwarves(array)
-  array.each_with_index do |value, index|
-    dwarves = "#{index.to_i+1}. #{value}"
-  end
-  puts "#{dwarves}"
-end
-
-def summon_captain_planet(array)
-  array.map! { |n| n.capitalize + "!"}
-    return "#{n}"
-  end
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+  i = 0
+  dwarves.each_with_index {|name, i
