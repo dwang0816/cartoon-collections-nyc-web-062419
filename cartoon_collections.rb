70248990f1187507f1c6e04193a624c63ffaee2d
dennis wang <dwang0816@gmail.com>
2019-06-12 03:42:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,3 +7,13 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map {|calls| calls.capitalize + "!"}
 end
 
+def long_planeteer_calls(calls)
+  calls.any? {|word| word.length > 4}
+end
+
+def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese|
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+  end
+end 
