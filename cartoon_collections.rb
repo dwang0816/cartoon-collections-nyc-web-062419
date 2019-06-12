1d44f78ca6ca51909f8c8e53b5dbd9b9ba407dfe
dennis wang <dwang0816@gmail.com>
2019-06-12 03:40:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,9 @@
 def roll_call_dwarves(array)
   i = 0
-  dwarves.each_with_index {|name, i
+  dwarves.each_with_index {|name, i| puts "#{i + 1}. #{name}"}
+end
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map {|calls| calls.capitalize + "!"}
+end
+
