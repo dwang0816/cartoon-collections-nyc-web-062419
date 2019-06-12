47f1c4f4441aa01ab3d1e8ce210de2a33d356e97
dennis wang <dwang0816@gmail.com>
2019-06-12 03:42:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(array)
   i = 0
-  dwarves.each_with_index {|name, i| puts "#{i + 1}. #{name}"}
+  array.each_with_index {|name, i| puts "#{i + 1}. #{name}"}
 end
 
 def summon_captain_planet(planeteer_calls)
